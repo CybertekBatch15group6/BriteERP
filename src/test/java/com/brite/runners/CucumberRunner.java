47b25c91390eca71d4491cdc7993eb479caa75f4
CananArikan <arikancnn@gmail.com>
2020-05-07 16:31:56 -0400

    added features directories

---
+++ src/test/java/com/brite/runners/CucumberRunner.java
@@ -0,0 +1,27 @@
+package com.brite.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+        glue = "com/brite/stepdefinitions",
+        features = "src/test/resources/features",
+        dryRun = false,
+        strict = false,
+        tags = "",
+        plugin = {
+                "html:target/default-report",
+                "json:target/cucumber1.json",
+                "rerun:target/rerun.txt"
+        }
+)
+
+
+
+
+
+public class CucumberRunner {
+}
