1d20328b9e9ba62e8743f93c85afa9d8f7621e57
samilAkkus <samilakgus15@email.com>
2020-05-10 22:46:44 -0700

    new runners added

---
+++ src/test/java/com/brite/runners/SmokeRunner.java
@@ -0,0 +1,23 @@
+package com.brite.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+        glue = "com/brite/stepdefinitions",
+        features = "src/test/resources/features",
+        dryRun = false,
+        strict = false,
+        tags = "@smoke",
+        plugin = {
+                "html:target/default-report",
+                "json:target/cucumber1.json",
+                "rerun:target/rerun.txt"
+        }
+)
+
+public class SmokeRunner {
+}
