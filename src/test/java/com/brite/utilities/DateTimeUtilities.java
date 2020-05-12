0da2a29c36a2bff666f64d433655315aa42b5767
Mukaddes K <mukaddes_06_@hotmail.com>
2020-05-11 22:48:13 -0500

    DateTime Utilities added

--- src/test/java/com/brite/utilities/DateTimeUtilities.java
+++ src/test/java/com/brite/utilities/DateTimeUtilities.java
@@ -1,4 +1,42 @@
 package com.brite.utilities;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class DateTimeUtilities {
+
+    /**
+     * This method returns current date as a string.
+     * Provide a format as a parameter
+     * dd - to specify day, like 01, 02, 03
+     *
+     * yyyy - to specify year like: 2010, 2020
+     *
+     * MM - to specify month like: 01, 02, 03,
+     * MMM - to specify month like: Jan, Feb, Mar
+     *
+     *
+     * @param format for example: MMM dd, yyyy = Mar 29, 2020
+     * @return current date as a string
+     *
+     * https://www.journaldev.com/17899/java-simpledateformat-java-date-format
+     */
+    public static String getCurrentDate(String format){
+        return LocalDate.now().format(DateTimeFormatter.ofPattern(format));
+    }
+
+    /**
+     * This method returns difference between end and start time
+     * @param start time
+     * @param end time
+     * @param format like h:m a --> 5:15 AM, 8:07 PM
+     * @return difference between end time and start time as a long
+     */
+    public static long getTimeDifference(String start, String end, String format){
+        LocalTime startTime = LocalTime.parse(start, DateTimeFormatter.ofPattern(format));
+        LocalTime endTime = LocalTime.parse(end, DateTimeFormatter.ofPattern(format));
+        return ChronoUnit.HOURS.between(startTime, endTime);
+    }
 }
