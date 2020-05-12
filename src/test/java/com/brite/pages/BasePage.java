60f153d5ec98ffb089e330680c882609bf84980e
samilAkkus <samilakgus15@email.com>
2020-05-11 23:35:29 -0700

    sales feature changes, new step defs, hook updated

--- src/test/java/com/brite/pages/BasePage.java
+++ src/test/java/com/brite/pages/BasePage.java
@@ -4,12 +4,24 @@ import com.brite.utilities.BrowserUtils;
 import com.brite.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasePage {
     protected WebDriver driver = Driver.getDriver();
     protected WebDriverWait wait = new WebDriverWait(driver,20);
+
+    @FindBy(xpath = "//span[@class='oe_topbar_name']")
+    protected WebElement curretUsername;
+    public String getCurrentUserName(){
+        BrowserUtils.wait(3);
+        wait.until(ExpectedConditions.visibilityOf(curretUsername));
+        return curretUsername.getText();
+    }
+
+
 
     public void navigation (String module){
         BrowserUtils.waitForPageToLoad(10);
--- src/test/java/com/brite/stepdefinitions/Hooks.java
+++ src/test/java/com/brite/stepdefinitions/Hooks.java
@@ -1,4 +1,34 @@
 package com.brite.stepdefinitions;
 
+import com.brite.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 public class Hooks {
+
+    @Before
+    public void setup(){
+
+        System.out.println("Test setup");
+        Driver.getDriver().manage().window().maximize();
+
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        //how to check if scenario failed
+        if(scenario.isFailed()){
+
+            TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+            byte[] image = takesScreenshot.getScreenshotAs(OutputType.BYTES);
+            //attach ss to the report if it fails
+            scenario.embed(image,"image/png",scenario.getName());
+        }
+        System.out.println("Test clean up");
+        Driver.closeDriver();
+
+    }
 }
